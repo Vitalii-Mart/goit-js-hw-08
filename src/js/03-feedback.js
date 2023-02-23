9f70ac9f0a7a2c26444904a2fd4a51b02488eaad
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-23 22:49:41 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,20 @@
+import throttle from "lodash.throttle";
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('textarea'),
+    input: document.querySelector('input'),
+};
+
+
+refs.form.addEventListener('input', qwe)
+
+
+function qwe(evt) {
+    const mes = evt.currentTarget.value;
+
+    localStorage.setItem('asd', mes);
+}
 
 
 
@@ -11,18 +28,17 @@
 
 
 
+// const formEl = document.querySelector(".login-form");
 
-const formEl = document.querySelector(".login-form");
+// formEl.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const {
+//     elements: { email, password },
+//   } = e.currentTarget;
+//   if (email.value === `` || password.value === ``) {
+//     return alert("Всі поля повинні бути заповнені!");
+//   }
+//   console.log(`Email: ${email.value}, Password: ${password.value}`);
 
-formEl.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const {
-    elements: { email, password },
-  } = e.currentTarget;
-  if (email.value === `` || password.value === ``) {
-    return alert("Всі поля повинні бути заповнені!");
-  }
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
-
-  e.currentTarget.reset();
-});
+//   e.currentTarget.reset();
+// });
