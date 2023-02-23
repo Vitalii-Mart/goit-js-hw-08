dd76d5998caec88e5da0431a4f647a29b79a0e7a
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-23 21:19:59 +0200

    foto

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+  if (email.value === `` || password.value === ``) {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  e.currentTarget.reset();
+});
