c05d19fa5addb0479940f55751055a08b3860798
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-23 20:50:07 +0000

    Deploying to gh-pages from @ Vitalii-Mart/goit-js-hw-08@9f70ac9f0a7a2c26444904a2fd4a51b02488eaad 🚀

--- 03-feedback.ad9c5abe.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".login-form").addEventListener("submit",(function(e){e.preventDefault();var r=e.currentTarget.elements,t=r.email,a=r.password;if(""===t.value||""===a.value)return alert("Всі поля повинні бути заповнені!");console.log("Email: ".concat(t.value,", Password: ").concat(a.value)),e.currentTarget.reset()}));
-//# sourceMappingURL=03-feedback.ad9c5abe.js.map
