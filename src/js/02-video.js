0f777e3be25cdd24f630779868d9aeb5d56a18f4
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-24 22:22:06 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,37 +1,22 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
+const TIME_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
 const currentTime = function (data) {
   localStorage.setItem(
-    'videoplayer-current-time',
+    TIME_KEY,
     JSON.stringify(data.seconds)
   );
 
-  let time = Number(localStorage.getItem('videoplayer-current-time'));
+  let time = Number(localStorage.getItem(TIME_KEY));
   console.log(time);
 };
 player.on('timeupdate', throttle(currentTime, 1000));
 
 player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(function (seconds) {})
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        break;
+  .setCurrentTime(localStorage.getItem(TIME_KEY));
 
-      default:
-        break;
-    }
-  });
